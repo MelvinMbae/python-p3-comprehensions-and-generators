a43cd7d8f42f9c875cfd14b76e84b8952b03e284
MelvinMbae <melvinmuthomi95@gmail.com>
2023-10-07 15:44:13 +0300

    remove comments

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -15,10 +15,10 @@ print(return_evens([1, 2, 3, 4, 5, 6, 7]))
 def make_exclamation(sentence_list):
     exclaimed_sentence = [word + "!" for word in sentence_list]
 
-    # for word in exclaimed_sentence:
-    #     print(word)
+    for word in exclaimed_sentence:
+        print(word)
 
-    return (exclaimed_sentence)
+    return exclaimed_sentence
 
 
 sentence_list = ["I like computers",
