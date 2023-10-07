cb3e61b52b8ab7a0d628ae63c1b5266071921608
MelvinMbae <melvinmuthomi95@gmail.com>
2023-10-07 15:43:42 +0300

    List comprehensions complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums = [num for num in num_list if num % 2 == 0]
+
+    for num in even_nums:
+        print(num, end='\n')
+
+    return even_nums
+
+
+print(return_evens([1, 2, 3, 4, 5, 6, 7]))
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_sentence = [word + "!" for word in sentence_list]
+
+    # for word in exclaimed_sentence:
+    #     print(word)
+
+    return (exclaimed_sentence)
+
+
+sentence_list = ["I like computers",
+                 "I require coffee", "Live long and prosper"]
+make_exclamation(sentence_list)
